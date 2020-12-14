ee1085dae773dd0939dd6c1027b0afb21e483a71
VictoriaSedig <victoria.sedig@gmail.com>
2020-12-14 15:22:34 +0100

    commentsadded

--- transmeth.m
+++ transmeth.m
@@ -1,6 +1,7 @@
 
+%%% Making all the matrices
 
-
+% Defining our active x for the constraints
 A = [-1 0 0 0 -1 0 0 0 -1 0 0 0; 
     0 -1 0 0 0 -1 0 0 0 -1 0 0; 
     0 0 -1 0 0 0 -1 0 0 0 -1 0; 
@@ -9,15 +10,22 @@ A = [-1 0 0 0 -1 0 0 0 -1 0 0 0;
     0 0 0 0 1 1 1 1 0 0 0 0;
     0 0 0 0 0 0 0 0 1 1 1 1];
 
-   
+%RHS of the constraint   
 b = [-15 -45 -30 -25 25 55 35]; %%%%% The 6th number is run for 55 but 60 in task 4
 
+% Obj func
 f = [10 0 20 11 12 7 9 20 0 14 16 18];
 lb = [0 0 0 0 0 0 0 0 0 0 0 0];
 ub = [inf inf inf inf inf inf inf inf inf inf inf inf];
 
+% Linprog with interior point method
 options = optimoptions('linprog','Algorithm','interior-point','display','iter');
 x_interiorpoint = linprog(f,A,b,[], [], lb, ub, options);  
 
+%Linprog with simplex method 
 options = optimoptions('linprog','Algorithm','dual-simplex','display','iter');
 x_simplex = linprog(f,A,b,[], [], lb, ub, options);
+
+
+
+
